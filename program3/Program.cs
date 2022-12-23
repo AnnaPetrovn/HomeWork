fca2a3d9d3c4928353a0fe70681cf54b99dbe39b
Anna <annapn5@mail.ru>
2022-12-23 20:29:22 +0600

    решила задание 3 к первому уроку

---
+++ program3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два 
+без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+void DividedIntoTwo()
+{   Console.WriteLine("Введите число");
+    string nu = Console.ReadLine();
+    int number = Convert.ToInt32(nu);//функция для перевода строки в целое число//
+    if (number%2==0) 
+    {
+        Console.WriteLine($"{number} делится на 2 без остатка");
+    }
+    else 
+    {
+        Console.WriteLine($"{number} не делится на 2 без остатка");
+    }    
+}
+DividedIntoTwo();
